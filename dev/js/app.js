d78c9a3937592d259fb11f9b80d7045b470adce4
GraysonP102 <78446269+GraysonP102@users.noreply.github.com>
2021-04-26 12:26:08 -0400

    Change var to let
    Changed var to let.

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,16 +1,16 @@
-var rotated = false;
-var list = document.getElementsByTagName(`td`);
+let rotated = false;
+let list = document.getElementsByTagName(`td`);
 for(i = 0; i<list.length; i++){
     list[i].addEventListener('click',rotation);
 }
 
 function rotation(event){
-    var target = event.target;
+    let target = event.target;
     if(rotated == false){
         target.style.transform = 'rotate(180deg)';
         rotated = true;
     }else{
         target.style.transform = 'rotate(360deg)';
         rotated = false;
-    };
+    }
 }

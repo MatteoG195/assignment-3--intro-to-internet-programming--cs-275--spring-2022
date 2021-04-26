31afa4d5e68f43539f8dc2fb073723bc5d0d83b5
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-26 18:16:33 -0400

    Define variable for each letter in table

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 window.onload = () => {
-    
+    let letterA = document.querySelectorAll(`td`)[0];
+    let letterB = document.querySelectorAll(`td`)[1];
+    let letterC = document.querySelectorAll(`td`)[2];
+    let letterD = document.querySelectorAll(`td`)[3];
+
+    console.log(letterA);
 };

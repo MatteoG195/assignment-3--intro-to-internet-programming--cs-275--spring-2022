4b8d714ad3a3abe3afdc8cf07d9c3d5ff7516e6a
GraysonP102 <78446269+GraysonP102@users.noreply.github.com>
2021-04-26 11:35:07 -0400

    Add primitive javascript
    Added javascript to add event listeners to each td and to rotate A 180 degrees on click.

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,1 +1,10 @@
-prompt('Hello');
+var list = document.getElementsByTagName(`td`);
+for(i = 0; i<list.length; i++){
+    list[i].addEventListener('click',rotation);
+}
+
+function rotation(){
+    var test = document.getElementsByTagName(`td`);
+    test[0].style.transform = 'rotate(180deg)';
+
+}

6529005a514737a48b0cf79cd3f722bd6801a9ec
EvanCopeland <78445857+EvanCopeland@users.noreply.github.com>
2021-04-28 15:47:17 -0400

    elements can be flipped

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var el_up = document.getElementById('flip');
+
+function flip() {
+    document.getElementById('flip').id = 'flipped'
+}
+
+function unflip() {
+    document.getElementById('flipped').id = 'flip'
+}

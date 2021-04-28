68ac2398f3ee8b63045ac36b2d68498595606933
GraysonP102 <78446269+GraysonP102@users.noreply.github.com>
2021-04-28 12:54:55 -0400

    Update animation
    Updated animation to fix a bug.

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,15 +1,12 @@
-let rotated = false;
 let list = document.getElementsByTagName(`td`);
 for(i = 0; i<list.length; i++){
     list[i].addEventListener('click',animate);
 }
 
 function animate(){
-    if(rotated==false){
+    if(!event.target.classList.contains(`animated`)){
         event.target.classList.add(`animated`);
-        rotated = true;
     }else{
         event.target.classList.add(`animated2`);
-        rotated = false;
     }
 }

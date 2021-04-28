2b29b8c4e492f095d1f90caf8b111b991956aa5b
GraysonP102 <78446269+GraysonP102@users.noreply.github.com>
2021-04-28 15:40:53 -0400

    Add clear function
    Added clear function such that letters may be clicked multiple times to repeat the animation.

--- dev/js/app.js
+++ dev/js/app.js
@@ -14,4 +14,20 @@ function animate(){
     }else{
         event.target.classList.add(`animated2`);
     }
+    for(i = 0; i< list.length; i++){
+        console.log(list[i]);
+    }
+    window.setTimeout(clear,500);
+    for(i = 0; i< list.length; i++){
+        console.log(list[i]);
+    }
+}
+
+function clear(){
+    for(i = 0; i<list.length; i++){
+        if(list[i].classList.contains(`animated2`)){
+            list[i].classList.remove(`animated`);
+            list[i].classList.remove(`animated2`);
+        }
+    }
 }

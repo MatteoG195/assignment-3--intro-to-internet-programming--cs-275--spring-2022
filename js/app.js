68847ff6a7bc42f96f85afd8f66b1f4234e2a93e
EvanCopeland <78445857+EvanCopeland@users.noreply.github.com>
2021-04-29 17:57:28 -0400

    items now can be exclusively toggled

--- js/app.js
+++ js/app.js
@@ -1,9 +1,45 @@
 var el_up = document.getElementById('flip');
+var clickedA = false;
+var clickedB = false;
+var clickedC = false;
+var clickedD = false;
 
-function flip() {
-    document.getElementById('flip').id = 'flipped'
+function flipA() {
+    if (clickedA) {
+        document.getElementById('flippedA').id = 'flipA'
+        clickedA = false;
+    } else {
+        document.getElementById('flipA').id = 'flippedA'
+        clickedA = true;
+    }
 }
 
-function unflip() {
-    document.getElementById('flipped').id = 'flip'
+function flipB() {
+    if (clickedB) {
+        document.getElementById('flippedB').id = 'flipB'
+        clickedB = false;
+    } else {
+        document.getElementById('flipB').id = 'flippedB'
+        clickedB = true;
+    }
+}
+
+function flipC() {
+    if (clickedC) {
+        document.getElementById('flippedC').id = 'flipC'
+        clickedC = false;
+    } else {
+        document.getElementById('flipC').id = 'flippedC'
+        clickedC = true;
+    }
+}
+
+function flipD() {
+    if (clickedD) {
+        document.getElementById('flippedD').id = 'flipD'
+        clickedD = false;
+    } else {
+        document.getElementById('flipD').id = 'flippedD'
+        clickedD = true;
+    }
 }

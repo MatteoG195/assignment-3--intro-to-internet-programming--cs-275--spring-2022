7327bc7dd76ea2b827f75d2da9d4ff1341c15fe0
Dylan LaPierre <56058450+dylanlap98@users.noreply.github.com>
2021-04-29 14:27:43 -0400

    Add gulp structure

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+const { src, dest, watch, series } = require(`gulp`);
+const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const cssLinter = require(`gulp-stylelint`);
+const browserSync = require(`browser-sync`);
+const jsLinter = require(`gulp-eslint`);
+const reload = browserSync.reload;
+
+
+let validateHTML = () => {
+    return src([
+        `html/*.html`,
+        `html/**/*.html`])
+        .pipe(htmlValidator());
+};
+
+let compressHTML = () => {
+    return src([`html/*.html`,`html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod/`));
+};
+
+let lintCSS = () => {
+    return src(`css/*.css`)
+        .pipe(cssLinter({
+            failAfterError: true,
+            reporters: [
+                {formatter: `verbose`, console: true}
+            ]
+        }))
+        .pipe(dest(`prod/`));
+};
+
+let lintJS = () => {
+    return src(`js/*.js`)
+        .pipe(jsLinter({
+            parserOptions: {
+                ecmaVersion: 2017,
+                sourceType: `module`
+            },
+            rules: {
+                indent: [2, 4, {SwitchCase: 1}],
+                quotes: [2, `backtick`],
+                semi: [2, `always`],
+                'linebreak-style': [2, `unix`],
+                'max-len': [1, 85, 4]
+            },
+            env: {
+                es6: true,
+                node: true,
+                browser: true
+            },
+            extends: `eslint:recommended`
+        }))
+        .pipe(jsLinter.formatEach(`compact`, process.stderr))
+        .pipe(dest(`prod/`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 0,
+        server: {
+            baseDir: [
+                `html`,
+                `css`,
+                'js',
+            ]
+        }
+    });
+
+    watch(`js/*.js`,
+        series(lintJS)
+    ).on(`change`, reload);
+
+    watch(`css/*.css`,
+        series(lintCSS)
+    ).on(`change`, reload);
+
+    watch(`html/*.html`,
+        series(validateHTML, compressHTML)
+    ).on(`change`, reload);
+};
+
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.lintCSS = lintCSS;
+exports.lintJS = lintJS;
+exports.serve = series(lintJS, validateHTML, compressHTML, lintCSS, serve);
+exports.build = series(lintJS, compressHTML, lintCSS);

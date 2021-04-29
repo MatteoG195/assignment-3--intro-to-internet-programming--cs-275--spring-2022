2c29384e4d48122b4cff933ba87a6e3fbf77bca9
aclavette <aclavette@users.noreply.github.com>
2021-04-29 18:10:50 -0400

    Finished final js draft

--- js/app.js
+++ js/app.js
@@ -3,40 +3,48 @@ let count = 0;
 
 window.onclick = e => {
     let cur = e.target;
-    console.log(count);
 
     if(cur.tagName != 'TD')
         alert("This element cannot be rotated.");
-    else {/*first click*/
-        if (count == 0)
-            firstRot(cur);
-        else /*every click after first*/
+    else {
+
+        //calls the proper function for every click after the first
+        if (count != 0)
             rot(cur);
+
+        //calls the proper function for the first click
+        else
+            firstRot(cur);
     }
 }
 
 
-
+//function for first node clicked
 function firstRot(e){
     e.classList.add("flipped");
     prev = e;
     count++;
 }
 
+//function for rotating clicked node and previous node
 function rot(e){
-    if (e.className == "flipped") {
-        e.classList.remove("flipped");
-        checkPrev(prev);
-        prev = e;
-    }
-    else {
-        e.classList.add("flipped");
-        checkPrev(prev);
-        prev = e;
-    }
+    if(e.compareDocumentPosition(prev) == 0)
+        check(e);
+    else
+        if (e.className == "flipped") {
+            e.classList.remove("flipped");
+            check(prev);
+            prev = e;
+        }
+        else {
+            e.classList.add("flipped");
+            check(prev);
+            prev = e;
+        }
 }
 
-function checkPrev(e){
+//checks and updates a node's rotation without assigning a previously clicked node
+function check(e){
     if(e.className == "flipped")
         e.classList.remove("flipped");
     else

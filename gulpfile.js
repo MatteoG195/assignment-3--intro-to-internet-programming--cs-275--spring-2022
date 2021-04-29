0120a383892d8b286d3420bc19b17be9bcf1fa74
cgaulin <cgaulin@hartford.edu>
2021-04-29 14:05:32 -0400

    Added lintJS task in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, series, watch } = require(`gulp`);
 const htmlCompressor = require(`gulp-htmlmin`);
 const cssCompressor = require(`gulp-clean-css`);
+const jsLinter = require(`gulp-eslint`);
 
 let compressHTML = () => {
     return src([`index.html`])
@@ -24,9 +25,16 @@ let transpileJSForDev = () => {
         .pipe(babel())
         .pipe(dest(`temp/js`));
 };
+let lintJS = () => {
+    return src(`js/*.js`)
+        .pipe(jsLinter(`.eslintrc.json`))
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
 exports.compressHTML = compressHTML;
 exports.compressCSS = compressCSS;
 exports.transpileJSForDev = transpileJSForDev;
+exports.lintJS = lintJS;
 
 
 

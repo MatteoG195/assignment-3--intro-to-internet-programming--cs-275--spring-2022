5f5702dabc8d1a5843936328da54d9429227a494
cgaulin <cgaulin@hartford.edu>
2021-04-29 10:01:53 -0400

    Added compressCSS task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, series, watch } = require(`gulp`);
 const htmlCompressor = require(`gulp-htmlmin`);
+const cssCompressor = require(`gulp-clean-css`);
 
 let compressHTML = () => {
     return src([`index.html`])
@@ -7,7 +8,19 @@ let compressHTML = () => {
         .pipe(dest(`prod/`));
 };
 
+let compressCSS = () => {
+    return src(`css/*.css`)
+        .pipe(cssCompressor({"debug": true}, (details) => {
+            console.log(`\n\tOriginal CSS file size: ` +
+                `${details.name}: ${details.stats.originalSize}`);
+            console.log(`\tMinified CSS file size: ` +
+                `${details.name}: ${details.stats.minifiedSize}\n`);
+        }))
+        .pipe(dest(`prod/css`));
+};
+
 exports.compressHTML = compressHTML;
+exports.compressCSS = compressCSS;
 
 
 

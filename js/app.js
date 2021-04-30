bca5ed93c3296bcddf36ec3bda551c778c5ea598
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-29 23:31:21 -0400

    Fixed Rotation Issue

--- js/app.js
+++ js/app.js
@@ -12,18 +12,75 @@ elemC.addEventListener("click", rotate3);
 elemD.addEventListener("click", rotate4);
 
 function rotate1(){
+    if(elemA.classList.contains("rotated")){
+        elemA.classList.remove("rotated");
+        elemA.classList.add("rotateback");
+    }else if(elemB.classList.contains("rotated")){
+        elemB.classList.remove("rotated");
+        elemB.classList.add("rotateback");
+        elemA.classList.add("rotated");
+    }else if(elemC.classList.contains("rotated")){
+        elemC.classList.remove("rotated");
+        elemC.classList.add("rotateback");
+        elemA.classList.add("rotated");
+    }else if(elemD.classList.contains("rotated")){
+        elemD.classList.remove("rotated");
+        elemD.classList.add("rotateback");
+        elemA.classList.add("rotated");
+    }else{
     elemA.classList.add("rotated");
+    }
 }
-
 function rotate2(){
-    elemA.classList.remove("rotated");
+    if(elemA.classList.contains("rotated")){
+        elemA.classList.remove("rotated");
+        elemA.classList.add("rotateback");
+        elemB.classList.add("rotated");
+    }else if(elemC.classList.contains("rotated")){
+        elemC.classList.remove("rotated");
+        elemC.classList.add("rotateback");
+        elemB.classList.add("rotated");
+    }else if(elemD.classList.contains("rotated")){
+        elemD.classList.remove("rotated");
+        elemD.classList.add("rotateback");
+        elemB.classList.add("rotated");
+    }else{
     elemB.classList.add("rotated");
+    }
 }
 
 function rotate3(){
+    if(elemA.classList.contains("rotated")){
+        elemA.classList.remove("rotated");
+        elemA.classList.add("rotateback");
+        elemC.classList.add("rotated");
+    }else if(elemB.classList.contains("rotated")){
+        elemB.classList.remove("rotated");
+        elemB.classList.add("rotateback");
+        elemC.classList.add("rotated");
+    }else if(elemD.classList.contains("rotated")){
+        elemD.classList.remove("rotated");
+        elemD.classList.add("rotateback");
+        elemC.classList.add("rotated");
+    }else{
     elemC.classList.add("rotated");
+    }
 }
 
 function rotate4(){
+    if(elemA.classList.contains("rotated")){
+        elemA.classList.remove("rotated");
+        elemA.classList.add("rotateback");
+        elemD.classList.add("rotated");
+    }else if(elemB.classList.contains("rotated")){
+        elemB.classList.remove("rotated");
+        elemB.classList.add("rotateback");
+        elemD.classList.add("rotated");
+    }else if(elemC.classList.contains("rotated")){
+        elemC.classList.remove("rotated");
+        elemC.classList.add("rotateback");
+        elemD.classList.add("rotated");
+    }else{
     elemD.classList.add("rotated");
+    }
 }

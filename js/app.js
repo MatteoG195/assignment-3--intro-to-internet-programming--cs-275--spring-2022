bd2a7616d1ee93abfa8c88aa8e85d4919d3e834f
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2021-04-30 16:12:54 -0400

    Fix bug with rotate2

--- js/app.js
+++ js/app.js
@@ -5,37 +5,40 @@ let b = cells[1]
 let c = cells[2]
 let d = cells[3]
 
-a.addEventListener(`click`,()=> {
+a.addEventListener(`click`, () => {
     rotate(a);
 });
 
-b.addEventListener(`click`,()=> {
+b.addEventListener(`click`, () => {
     rotate(b);
 });
 
-c.addEventListener(`click`,()=> {
+c.addEventListener(`click`, () => {
     rotate(c);
 });
 
-d.addEventListener(`click`,()=> {
+d.addEventListener(`click`, () => {
     rotate(d);
 });
 
 function rotate(e) {
-    if (e.classList.contains(`rotated`)){
+    if (e.classList.contains(`rotated2`)) {
+        e.classList.remove(`rotated2`);
+    }
+    if (e.classList.contains(`rotated`)) {
         e.classList.remove(`rotated`);
         e.classList.add(`rotated2`);
         clear();
-    }
-    else {
+
+    } else {
         clear();
         e.classList.add(`rotated`);
     }
 }
 
 function clear() {
-    for(let i = 0; i < cells.length; i++){
-        if (cells[i].classList.contains(`rotated`)){
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].classList.contains(`rotated`)) {
             cells[i].classList.remove(`rotated`);
         }
     }

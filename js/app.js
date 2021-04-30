876127975120ab0d691141eac3f92e190ff492d0
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2021-04-30 14:25:31 -0400

    Add eventListeners

--- js/app.js
+++ js/app.js
@@ -3,3 +3,22 @@ let a = cells[0]
 let b = cells[1]
 let c = cells[2]
 let d = cells[3]
+
+a.addEventListener(`click`,()=> {
+    rotate(a);
+});
+
+b.addEventListener(`click`,()=> {
+    rotate(b);
+});
+
+c.addEventListener(`click`,()=> {
+    rotate(c);
+});
+
+d.addEventListener(`click`,()=> {
+    rotate(d);
+});
+
+function rotate(e) {
+}

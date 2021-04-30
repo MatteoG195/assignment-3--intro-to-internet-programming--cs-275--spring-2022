7721809822735e913d7e38015de0b9c5e8350597
mcarpente <78821917+mcarpente@users.noreply.github.com>
2021-04-30 16:46:04 -0400

    add Flip EventListener For A,B,C,D

--- js/app.js
+++ js/app.js
@@ -6,7 +6,50 @@ let B = x[1];
 let C = x[2];
 let D = x[3];
 
-//A.classList.toggle(`rotate`);
+function rotate(letter) {
+    letter.classList.toggle(`rotate`);
+}
+
+A.addEventListener(`click`, ()=>{
+    rotate(A);
+    if (B.classList.contains(`rotate`))
+        rotate(B);
+    if (C.classList.contains(`rotate`))
+        rotate(C);
+    if (D.classList.contains(`rotate`))
+        rotate(D);
+});
+
+B.addEventListener(`click`, ()=>{
+    rotate(B);
+    if (A.classList.contains(`rotate`))
+        rotate(A);
+    if (C.classList.contains(`rotate`))
+        rotate(C);
+    if (D.classList.contains(`rotate`))
+        rotate(D);
+});
+
+C.addEventListener(`click`, ()=>{
+    rotate(C);
+    if (A.classList.contains(`rotate`))
+        rotate(A);
+    if (B.classList.contains(`rotate`))
+        rotate(B);
+    if (D.classList.contains(`rotate`))
+        rotate(D);
+});
+
+D.addEventListener(`click`, ()=>{
+    rotate(D);
+    if (A.classList.contains(`rotate`))
+        rotate(A);
+    if (B.classList.contains(`rotate`))
+        rotate(B);
+    if (C.classList.contains(`rotate`))
+        rotate(C);
+});
+
 
 
 

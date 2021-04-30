46013fd9569a1f8e428237012b26ec4d47578508
Youmeng Hin <youmenghin@gmail.com>
2021-04-30 04:18:04 -0400

    Add event listen to each element
    Invoke the flip function when clicked.

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,9 @@
 windows.onload = () => {
 
   let tagTD = document.querySelectorAll("td");
+  tagTD[0].addEventListener(`click`, flip);     //Event listener when clicked calls the flip function
+  tagTD[1].addEventListener(`click`, flip);
+  tagTD[2].addEventListener(`click`, flip);
+  tagTD[3].addEventListener(`click`, flip);
 
-}
+};

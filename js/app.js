1fea444a72b7e5b74fa0de85b629746a20e83025
Nicholas Theroux <78562198+Nick-Theroux@users.noreply.github.com>
2021-04-29 23:15:04 -0400

    Moved rotation Function
    Moved rotation function below declarations for easier reading of code.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 window.onload=()=>{
 
-    function rotate(letter){
-      letter.classList.add('flip');  
-    }
     var A = document.querySelectorAll('td')[0];
     var B = document.querySelectorAll('td')[1];
     var C = document.querySelectorAll('td')[2];
     var D = document.querySelectorAll('td')[3];
     var temp = 'Null';
+
+    function rotate(letter){
+            letter.classList.add('flip');
+    }
 
     A.addEventListener('click',()=>{
         rotate(A);

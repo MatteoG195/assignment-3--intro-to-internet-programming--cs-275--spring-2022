f19052ecf662969200dd9c1f1ac871693ac58d93
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-29 22:15:14 -0400

    Add rotation using CSS and JS

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
-var elem = document.getElementById("A");
+var elemA = document.getElementById("A");
+var elemB = document.getElementById("B");
+var elemC = document.getElementById("C");
+var elemD = document.getElementById("D");
+
 
 //for(var i = 0;i < elem.length;i++){
 //elem[i].addEventListener("click", rotater);
+elemA.addEventListener("click", rotate1);
+elemB.addEventListener("click", rotate2);
+elemC.addEventListener("click", rotate3);
+elemD.addEventListener("click", rotate4);
 
-
-function rotater(){
-    elem.style.transition = "all 3s";
-    elem.style.transform = "rotate(180deg)";
-
+function rotate1(){
+    elemA.classList.add("rotated");
 }
-//for(var i = 0;i < elem.length;i++){
-//elem[i].addEventListener("click", rotater);
-//}
 
-elem.addEventListener("click", rotater);
+function rotate2(){
+    elemA.classList.remove("rotated");
+    elemB.classList.add("rotated");
+}
+
+function rotate3(){
+    elemC.classList.add("rotated");
+}
+
+function rotate4(){
+    elemD.classList.add("rotated");
+}

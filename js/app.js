5767aefb1bbd678530152225d38aa6ab6e449622
cgaulin <cgaulin@hartford.edu>
2021-04-30 18:48:16 -0400

    created a variable to hold all letters using querySelectorAll

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 window.onload = () => {
     console.log(`%c JavaScript works!`,
         `background-color: yellow; font-weight: bold;`);
+
+    let allLetters = document.querySelectorAll(`td`);
 };

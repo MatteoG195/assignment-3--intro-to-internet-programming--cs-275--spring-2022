c33eb2ba566e0f0b4c87239bcc7392268a1cc357
Dylan LaPierre <56058450+dylanlap98@users.noreply.github.com>
2021-05-03 14:39:59 -0400

    Added Keyframes and changed variable names for readability

--- js/app.js
+++ js/app.js
@@ -6,22 +6,22 @@ for(let i = 0; i < list.length; i++){
 function animate(){
     for(let i = 0; i<list.length; i++){
         if(list[i] != event.target && list[i]classList.contains('animated')) {
-            list[i].classList.add('animated2');
+            list[i].classList.add('animated_2');
         }
     }
     if(!event.target.classList.contains('animated')) {
         event.target.classList.add('animated');
     } else {
-        event.target.classList.add('animated2');
+        event.target.classList.add('animated_2');
     }
     window.setTimeout(clear_window, 10);
 }
 
 function clear_window(){
     for(let i = 0; i<list.length; i++){
-        if(list[i].classList.contains(`animated2`)){
+        if(list[i].classList.contains(`animated_2`)){
             list[i].classList.remove(`animated`);
-            list[i].classList.remove(`animated2`);
+            list[i].classList.remove(`animated_2`);
         }
     }
 }

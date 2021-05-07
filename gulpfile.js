4e13f3220b65ada9e44055641ad196a81ec0683e
tejasv378 <venkatesw@hartford.edu>
2021-05-07 18:01:40 -0400

    Added compressHTML task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 const { src, dest, watch, series} = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
 
 let validateHTML = () => {
     return src(`src/*.html`)
         .pipe(htmlValidator());
 };
 
+let compressHTML = () => {
+    return src(`src/*.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod/`));
+};
+
+
+
 exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;

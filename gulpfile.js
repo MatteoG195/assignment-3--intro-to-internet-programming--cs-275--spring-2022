2a639b4ca322f45348c70df025ac8f426bb570d6
tejasv378 <venkatesw@hartford.edu>
2021-05-07 18:54:01 -0400

    Added lintJS task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ const sass = require(`gulp-sass`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
 const cssLinter = require(`gulp-stylelint`);
+const babel = require(`gulp-babel`);
+const jsLinter = require(`gulp-eslint`);
+const jsCompressor = require(`gulp-uglify`);
 
 let validateHTML = () => {
     return src(`src/*.html`)
@@ -34,7 +37,33 @@ let compileCSSForProd = () => {
         .pipe(dest(`prod/css`));
 };
 
+let lintJS = () => {
+    return src(`src/js/*.js`)
+        .pipe(jsLinter({
+            parserOptions: {
+                ecmaVersion: 2017,
+                sourceType: `module`
+            },
+            rules: {
+                indent: [2, 4, {SwitchCase: 1}],
+                quotes: [2, `backtick`],
+                semi: [2, `always`],
+                'linebreak-style': [2, `unix`],
+                'max-len': [1, 85, 4]
+            },
+            env: {
+                es6: true,
+                node: true,
+                browser: true
+            },
+            extends: `eslint:recommended`
+        }))
+        .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+
+
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
 exports.lintCSS = lintCSS;
 exports.compileCSSForProd = compileCSSForProd;
+exports.lintJS = lintJS;

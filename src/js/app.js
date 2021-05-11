7654d1ea4565dc1b96883be58121515d1ff44173
tejasv378 <venkatesw@hartford.edu>
2021-05-10 23:27:13 -0400

    Added querySelectorAll to each letter

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 window.onload = () => {
+let A = document.querySelectorAll(`td`) [0];
+let B = document.querySelectorAll(`td`) [1];
+let C = document.querySelectorAll(`td`) [2];
+let D = document.querySelectorAll(`td`) [3];
 
 };
